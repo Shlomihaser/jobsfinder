97793e4a32acc99b8788fa29831e485dbe0e9e31
ShlomiHaser <hasshlomi21@gmail.com>
2026-02-14 21:24:38 +0200

    initial project setup

---
+++ app/core/config.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+    DATABASE_URL: str
+    
+    RABBITMQ_USER: str
+    RABBITMQ_PASS: str
+    RABBITMQ_URL: str
+    
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+settings = Settings()
